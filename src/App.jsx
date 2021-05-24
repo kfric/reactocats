f3c0b39b54cdc2f762f602c6287ac30b7d158519
Karl Frick <karl.frick5@gmail.com>
2021-05-24 17:58:18 -0400

    Added html as jsx and css as scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,41 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <article>
+        <div>
+          <a href="https://octodex.github.com//spectrocat/">
+            <img
+              className="cat"
+              src="https://octodex.github.com//images/spectrocat.png"
+              width="290"
+              height="290"
+              alt="Spectrocat"
+            />
+          </a>
+        </div>
+        <ul>
+          <li>
+            #44:
+            <a
+              className="catName"
+              href="https://octodex.github.com//images/spectrocat.png"
+            >
+              <strong>Spectrocat</strong>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/cameronmcefee">
+              <img
+                src="https://github.com/cameronmcefee.png"
+                width="24px"
+                height="24px"
+                alt="cameronmcefee"
+              />
+            </a>
+          </li>
+        </ul>
+      </article>
+    )
   }
 }
