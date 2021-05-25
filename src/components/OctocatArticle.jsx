1fa82696e96157b6511390a9b569f85ad40626bc
Karl Frick <karl.frick5@gmail.com>
2021-05-24 20:54:29 -0400

    Using properties of OctocatArticle as temp to display all of the octocats

--- src/components/OctocatArticle.jsx
+++ src/components/OctocatArticle.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 
 export class OctocatArticle extends React.Component {
   render() {
-    return <div>something</div>
+    return (
+      <article>
+        <main>
+          <section>
+            <article>
+              <div>
+                <a href={this.props.imgLink}>
+                  <img
+                    className="cat"
+                    src={this.props.catImg}
+                    width="290"
+                    height="290"
+                    alt={this.props.catName}
+                  />
+                </a>
+              </div>
+              <ul>
+                <li>
+                  #{this.props.Id}:
+                  <a className="catName" href={this.props.catName}>
+                    <strong>{this.props.catName}</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href={this.props.artistLink}>
+                    <img
+                      src={this.props.artistImg}
+                      width="24px"
+                      height="24px"
+                      alt={this.props.artistName}
+                    />
+                  </a>
+                </li>
+              </ul>
+            </article>
+          </section>
+        </main>
+      </article>
+    )
   }
 }
